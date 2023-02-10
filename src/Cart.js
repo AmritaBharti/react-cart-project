ea549a317556d71c38939e203ceceecf809d06ad
Amrita <amritasharma1214@gmail.com>
2023-02-10 12:49:49 +0530

    added increase decrease quantity function, handled the issue after adding props

--- src/Cart.js
+++ src/Cart.js
@@ -32,24 +32,56 @@ class Cart extends React.Component {
         
     }
 
+    handleIncreaseQuantity = (product) => {
+        console.log('please increase the quantity of the product', product);
+        const {products} = this.state;
+        const index = products.indexOf(product);
+
+        products[index].qty += 1;
+
+        this.setState({
+            products:products
+        })
+
+    }
+
+    handleDecreaseQuantity = (product) => {
+        console.log('please decrease the quantity of the product', product);
+        const {products} = this.state;
+        const index = products.indexOf(product);
+
+        if( products[index].qty == 0){
+            return ;
+        }
+
+        products[index].qty -= 1;
+
+        this.setState({
+            products:products
+        })
+    }
+
     render() {
         const {products} = this.state
         return (
             <div className="cart">
                {products.map( (product)=>{
-                return <CartItem product={product} key={product.id}/>
-               } )} 
+                return (
+                    <CartItem 
+                    product={product}
+                     key={product.id}
+                     onIncreaseQuantity ={this.handleIncreaseQuantity} //just giving reference to the function
+                     onDecreaseQuantity = {this.handleDecreaseQuantity}
+                     />
+                )     
+               })} 
             </div>
 
         );
-
     }
-
 }
 
 export default Cart;
-
-
 
 // render() {
 //     const arr = [1, 2, 3, 4, 5, 6, 7];
--- src/CartItems.js
+++ src/CartItems.js
@@ -15,41 +15,41 @@ class CartItem extends React.Component {
     //     //this.increaseQunatity = this.increaseQunatity.bind(this);
     // }
 
-    increaseQunatity=()=>{
-       console.log('this.state',this.state) // when we attach event listener we are passing a reference to our increaseQunatity function and when it internally calls it the value of this get lost -> we can solve this by binding -> binding .bind()
-        // another way is we can bind this in constructor function
-        //or we can use arrow function it will automatically bind the value of this
+    // increaseQunatity=()=>{
+    //    console.log('this.state',this.state) // when we attach event listener we are passing a reference to our increaseQunatity function and when it internally calls it the value of this get lost -> we can solve this by binding -> binding .bind()
+    //     // another way is we can bind this in constructor function
+    //     //or we can use arrow function it will automatically bind the value of this
         
-        //this.state.qty +=1; // it will incr object qty but it will not shown in ui -> to show in ui -> we have to incr qty in state not only on object and re-render it
-        //by using set state function two way to do it
+    //     //this.state.qty +=1; // it will incr object qty but it will not shown in ui -> to show in ui -> we have to incr qty in state not only on object and re-render it
+    //     //by using set state function two way to do it
 
-        // //set state form 1 -> we will give an object
-        // this.setState(  // this set state function call will triger re-render of the component with update value 
-        //     {
-        //         qty:this.state.qty + 1  // shallow merging -> it will not tough other property
-        //     },() =>{} call function as second argument
-        // ) ; //it is getting inherited from component class in react
+    //     // //set state form 1 -> we will give an object
+    //     // this.setState(  // this set state function call will triger re-render of the component with update value 
+    //     //     {
+    //     //         qty:this.state.qty + 1  // shallow merging -> it will not tough other property
+    //     //     },() =>{} call function as second argument
+    //     // ) ; //it is getting inherited from component class in react
         
-        //// set state form 2 -> we will pass call back function -> we can use when we reruire previous state too
-        this.setState((prevState)=>{
-            return {
-                qty :prevState.qty + 1
-            }
-        });  //it is getting inherited from component class in react
-    }
+    //     //// set state form 2 -> we will pass call back function -> we can use when we reruire previous state too
+    //     this.setState((prevState)=>{
+    //         return {
+    //             qty :prevState.qty + 1
+    //         }
+    //     });  //it is getting inherited from component class in react
+    // }
 
-    decreaseQunatity=()=>{
-        const {qty}= this.state
-        if(qty == 0){
-            return;
-         }
-        this.setState((prevState)=>{
-                return {
-                    qty : prevState.qty - 1
-                }
+    // decreaseQunatity=()=>{
+    //     const {qty}= this.state
+    //     if(qty == 0){
+    //         return;
+    //      }
+    //     this.setState((prevState)=>{
+    //             return {
+    //                 qty : prevState.qty - 1
+    //             }
 
-        });  
-    }
+    //     });  
+    // }
 
     //for a class component to be a react component we need to give render method and that render method will return JSX which will describe the ui for the component
     render() {
@@ -71,13 +71,13 @@ class CartItem extends React.Component {
                             alt="increase"
                             className="action-icons"
                             src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg"
-                            onClick={this.increaseQunatity}
+                            onClick={()=> this.props.onIncreaseQuantity(this.props.product)}
                         />
                         <img
                             alt="decrease"
                             className="action-icons"
                             src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"
-                            onClick={this.decreaseQunatity}
+                            onClick={()=> this.props.onDecreaseQuantity(this.props.product)}
                         />
                         <img
                             alt="delete"
